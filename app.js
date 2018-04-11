8f05d1399f1084e10c4da8165bfca77c159fd0d2
CarlosFTG <37017033+CarlosFTG@users.noreply.github.com>
2018-04-11 19:35:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ var namesGenerator=require('./namesGenerator');
 
 var mongoose = require('mongoose');
 
-//var model=require('./model').Movie;
 
-var Movie=require('./model');
+
+var Model=require('./model');
+
+
+mongoose.connect("mongodb://localhost:27017/movies_exercise", function(err) {
+    if (err){
+        console.log("Error connecting with DB")
+    }else{console.log('DB connected')}
+});
 
 //looping through the moviesObject list created in namesGenerator.js and inserting every single object in the collection
 for (let i=0;i<namesGenerator.length;i++){
-	
-    	var movie= new Movie({movie_name:namesGenerator[i].movie_name,director:namesGenerator[i].director, year:namesGenerator[i].year});
+
+    	var movie= new Model({movie_name:namesGenerator[i].movie_name,director:namesGenerator[i].director, year:namesGenerator[i].year});
 
     	movie.save(function (err) {
         if (err){
         	console.log(err)
         }else{console.log('movie '+ [i]+' inserted')}
-            
-      })
-      
+         mongoose.connection.close()
+      	
 
+    })
     }
